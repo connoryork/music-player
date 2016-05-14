46a86d99812e9d311920ff4e3e08edf26b68500b
mattBroman <matthew.broman@yahoo.com>
2016-05-13 20:46:25 -0500

    made rewind to start work when the clip is running

--- src/MusicPlayerModel.java
+++ src/MusicPlayerModel.java
@@ -145,7 +145,7 @@ public class MusicPlayerModel extends Observable {
      *
      */
     public void setSongPosition(int changed) {
-        if(this.clip.isRunning() && this.clip != null) {
+        if(this.clip != null) {
             this.clip.stop();
             this.clip.setFramePosition(this.clip.getFramePosition() - changed);
             this.clip.start();
